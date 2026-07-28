f21e25049bc505160b23117c35656dcd60784812
Viswanadha Manikanta Karthik <viswanadhakarthik9@gmail.com>
2026-07-28 20:54:40 +0530

    Updated products and UI

--- src/data/ProductData.jsx
+++ src/data/ProductData.jsx
@@ -2,20 +2,63 @@ import shoe1 from "../assets/shoe/shoe1.jpg";
 import shoe2 from "../assets/shoe/shoe2.jpg";
 import shoe3 from "../assets/shoe/shoe3.jpg";
 import shoe4 from "../assets/shoe/shoe4.jpg";
+import shoe5 from "../assets/shoe/shoe5.jpg";
 import shoe6 from "../assets/shoe/shoe6.jpg";
+import shoe7 from "../assets/shoe/shoe7.jpg";
 import shoe8 from "../assets/shoe/shoe8.jpg";
 import shoe9 from "../assets/shoe/shoe9.jpg";
 import shoe10 from "../assets/shoe/shoe10.jpg";
 import shoe11 from "../assets/shoe/shoe11.jpg";
 import shoe12 from "../assets/shoe/shoe12.jpg";
 import shoe13 from "../assets/shoe/shoe13.jpg";
+import shoe14 from "../assets/shoe/shoe14.jpg";
 import shoe15 from "../assets/shoe/shoe15.jpg";
 import shoe16 from "../assets/shoe/shoe16.jpg";
+import shoe17 from "../assets/shoe/shoe17.jpg";
+import shoe18 from "../assets/shoe/shoe18.jpg";
+import shoe19 from "../assets/shoe/shoe19.jpg";
 import shoe20 from "../assets/shoe/shoe20.jpg";
 import shoe21 from "../assets/shoe/shoe21.jpg";
+import shoe22 from "../assets/shoe/shoe22.jpg";
+import shoe23 from "../assets/shoe/shoe23.jpg";
+import shoe24 from "../assets/shoe/shoe24.jpg";
+import shoe25 from "../assets/shoe/shoe25.jpg";
+import shoe26 from "../assets/shoe/shoe26.jpg";
+import shoe27 from "../assets/shoe/shoe27.jpg";
+
+import shoe29 from "../assets/shoe/shoe29.jpg";
+import shoe30 from "../assets/shoe/shoe30.jpg";
+import shoe31 from "../assets/shoe/shoe31.jpg";
+import shoe32 from "../assets/shoe/shoe32.jpg";
+import shoe33 from "../assets/shoe/shoe33.jpg";
+import shoe34 from "../assets/shoe/shoe34.jpg";
+import shoe35 from "../assets/shoe/shoe35.jpg";
+import shoe36 from "../assets/shoe/shoe36.jpg";
+import shoe37 from "../assets/shoe/shoe37.jpg";
+import shoe38 from "../assets/shoe/shoe38.jpg";
+import shoe39 from "../assets/shoe/shoe39.jpg";
+import shoe40 from "../assets/shoe/shoe40.jpg";
+import shoe41 from "../assets/shoe/shoe41.jpg";
+import shoe42 from "../assets/shoe/shoe42.jpg";
+import shoe43 from "../assets/shoe/shoe43.jpg";
+import shoe44 from "../assets/shoe/shoe44.jpg";
+import shoe45 from "../assets/shoe/shoe45.jpg";
 
 import tshirt1 from "../assets/shoe/tshirt1.jpg";
 import tshirt2 from "../assets/shoe/tshirt2.jpg";
+import tshirt4 from "../assets/shoe/tshirt4.jpg";
+import tshirt5 from "../assets/shoe/tshirt5.jpg";
+import tshirt6 from "../assets/shoe/tshirt6.jpg";
+import tshirt7 from "../assets/shoe/tshirt7.jpg";
+import tshirt8 from "../assets/shoe/tshirt8.jpg";
+import tshirt9 from "../assets/shoe/tshirt9.jpg";
+import tshirt10 from "../assets/shoe/tshirt10.jpg";
+import tshirt11 from "../assets/shoe/tshirt11.jpg";
+import tshirt12 from "../assets/shoe/tshirt12.jpg";
+import tshirt13 from "../assets/shoe/tshirt13.jpg";
+import tshirt14 from "../assets/shoe/tshirt14.jpg";
+import tshirt15 from "../assets/shoe/tshirt15.jpg";
+import tshirt16 from "../assets/shoe/tshirt16.jpg";
 
 const Products = [
   {
@@ -86,21 +129,21 @@ const Products = [
     name: "Nike Blazer",
     price: 8999,
     image: shoe10,
-    category: "kids",
+    category: "men",
   },
   {
     id: 11,
     name: "Nike Dunk Low",
     price: 9499,
     image: shoe11,
-    category: "kids",
+    category: "men",
   },
   {
     id: 12,
     name: "Nike Alphafly",
     price: 22999,
     image: shoe12,
-    category: "kids",
+    category: "men",
   },
   {
     id: 13,
@@ -114,14 +157,14 @@ const Products = [
     name: "Nike Structure",
     price: 8799,
     image: shoe10,
-    category: "women",
+    category: "men",
   },
   {
     id: 15,
     name: "Nike Flex Run",
     price: 5499,
     image: shoe15,
-    category: "kids",
+    category: "men",
   },
   {
     id: 16,
@@ -144,6 +187,293 @@ const Products = [
     image: shoe21,
     category: "women",
   },
+  {
+  id: 19,
+  name: "Nike Air Zoom",
+  price: 9999,
+  image: shoe17,
+  category: "women",
+},
+{
+  id: 20,
+  name: "Nike Downshifter",
+  price: 6499,
+  image: shoe18,
+  category: "women",
+},
+{
+  id: 21,
+  name: "Nike React Infinity",
+  price: 11999,
+  image: shoe19,
+  category: "women",
+},
+{
+  id: 22,
+  name: "Nike Motiva",
+  price: 10499,
+  image: shoe20,
+  category: "women",
+},
+{
+  id: 23,
+  name: "Nike Cosmic Runner",
+  price: 5899,
+  image: shoe21,
+  category: "women",
+},
+{
+  id: 24,
+  name: "Nike Zoom Fly",
+  price: 14999,
+  image: shoe22,
+  category: "men",
+},
+{
+  id: 25,
+  name: "Nike Free Run",
+  price: 7299,
+  image: shoe23,
+  category: "men",
+},
+{
+  id: 26,
+  name: "Nike Air Zoom GT",
+  price: 13499,
+  image: shoe24,
+  category: "men",
+},
+{
+  id: 27,
+  name: "Nike LeBron XXI",
+  price: 16999,
+  image: shoe25,
+  category: "men",
+},
+{
+  id: 28,
+  name: "Nike KD 17",
+  price: 15999,
+  image: shoe26,
+  category: "men",
+},
+{
+  id: 29,
+  name: "Nike Giannis Freak",
+  price: 13999,
+  image: shoe27,
+  category: "men",
+},
+{
+  id: 30,
+  name: "Nike Sabrina",
+  price: 12999,
+  image: shoe43,
+  category: "women",
+},
+{
+  id: 31,
+  name: "Nike Metcon",
+  price: 10999,
+  image: shoe29,
+  category: "men",
+},
+{
+  id: 32,
+  name: "Nike SuperRep",
+  price: 9499,
+  image: shoe30,
+  category: "women",
+},
+{
+  id: 33,
+  name: "Nike Court Vision",
+  price: 6999,
+  image: shoe31,
+  category: "women",
+},
+{
+  id: 34,
+  name: "Nike Air Jordan",
+  price: 18999,
+  image: shoe32,
+  category: "women",
+},
+{
+  id: 35,
+  name: "Nike Zoom Rival",
+  price: 8799,
+  image: shoe33,
+  category: "women",
+},
+{
+  id: 36,
+  name: "Nike Tempo Next%",
+  price: 19999,
+  image: shoe34,
+  category: "women",
+},
+{
+  id: 37,
+  name: "Nike Zoom Structure",
+  price: 9999,
+  image: shoe35,
+  category: "women",
+},
+{
+  id: 38,
+  name: "Nike Precision",
+  price: 7499,
+  image: shoe36,
+  category: "women",
+},
+{
+  id: 39,
+  name: "Nike Renew Ride",
+  price: 8199,
+  image: shoe37,
+  category: "women",
+},
+{
+  id: 40,
+  name: "Nike Legend Essential",
+  price: 6799,
+  image: shoe38,
+  category: "women",
+},
+{
+  id: 41,
+  name: "Nike Star Runner",
+  price: 5999,
+  image: shoe39,
+  category: "kids",
+},
+{
+  id: 42,
+  name: "Nike Zoom Gravity",
+  price: 9499,
+  image: shoe41,
+  category: "kids",
+},
+{
+  id: 43,
+  name: "Nike React Escape",
+  price: 10999,
+  image: shoe42,
+  category: "kids",
+},
+{
+  id: 44,
+  name: "Nike Dri-FIT Tee",
+  price: 2499,
+  image: tshirt4,
+  category: "tshirt",
+},
+{
+  id: 45,
+  name: "Nike Sportswear Tee",
+  price: 2899,
+  image: tshirt5,
+  category: "tshirt",
+},
+{
+  id: 46,
+  name: "Nike Club Essentials",
+  price: 2199,
+  image: tshirt6,
+  category: "tshirt",
+},
+{
+  id: 47,
+  name: "Nike Training Tee",
+  price: 2699,
+  image: tshirt7,
+  category: "tshirt",
+},
+{
+  id: 48,
+  name: "Nike Running Tee",
+  price: 2999,
+  image: tshirt8,
+  category: "tshirt",
+},
+{
+  id: 49,
+  name: "Nike Heritage Tee",
+  price: 3299,
+  image: tshirt9,
+  category: "tshirt",
+},
+{
+  id: 50,
+  name: "Nike Sportswear Max90",
+  price: 3499,
+  image: tshirt10,
+  category: "tshirt",
+},
+{
+  id: 51,
+  name: "Nike Pro Compression",
+  price: 3999,
+  image: tshirt11,
+  category: "tshirt",
+},
+{
+  id: 52,
+  name: "Nike Yoga Tee",
+  price: 2599,
+  image: tshirt12,
+  category: "tshirt",
+},
+{
+  id: 53,
+  name: "Nike Everyday Tee",
+  price: 2299,
+  image: tshirt13,
+  category: "tshirt",
+},
+{
+  id: 54,
+  name: "Nike Graphic Tee",
+  price: 2799,
+  image: tshirt14,
+  category: "tshirt",
+},
+{
+  id: 55,
+  name: "Nike Premium Tee",
+  price: 3799,
+  image: tshirt15,
+  category: "tshirt",
+},
+{
+  id: 56,
+  name: "Nike Oversized Tee",
+  price: 3199,
+  image: tshirt16,
+  category: "tshirt",
+},
+{
+  id: 57,
+  name: "Nike Kids Revolution",
+  price: 4999,
+  image: shoe43,
+  category: "kids",
+},
+{
+  id: 58,
+  name: "Nike Kids Air Zoom",
+  price: 5499,
+  image: shoe44,
+  category: "kids",
+},
+{
+  id: 59,
+  name: "Nike Kids Flex Runner",
+  price: 5999,
+  image: shoe45,
+  category: "kids",
+},
 ];
 
 export default Products;
